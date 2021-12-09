7a478c65bcb0d68c8287564e21f2d6d46dd26a65
Sam Forbyn <samforbyn@gmail.com>
2021-12-09 13:09:32 -0700

    matplotlib imported

--- data_presenter.py
+++ data_presenter.py
@@ -1,34 +1,34 @@
 import csv
 from decimal import Decimal
-
+import matplotlib as plt
 # 1
 open_file = open("CupcakeInvoices.csv")
-for line in open_file:
-    print(line)
+# for line in open_file:
+#     print(line)
 
 # 2
 myCsv = csv.reader(open_file)
-for line in myCsv:
-    print(line[2])
-open_file.seek(0,0)
+# for line in myCsv:
+#     print(line[2])
+# open_file.seek(0,0)
 #3
-for line in myCsv:
-    quant = float(line[3])
-    price = float(line[4])
-    ans = quant*price
-    format_ans = "{:.2f}".format(ans)
-    print(f'Total for {line[0]} is {format_ans}')
+# for line in myCsv:
+#     quant = float(line[3])
+#     price = float(line[4])
+#     ans = quant*price
+#     format_ans = "{:.2f}".format(ans)
+#     print(f'Total for {line[0]} is {format_ans}')
 
 #4
-open_file.seek(0,0)
-ans = 0
-for line in myCsv:
-    quant = float(line[3])
-    price = float(line[4])
-    ans += quant*price
+# open_file.seek(0,0)
+# ans = 0
+# for line in myCsv:
+#     quant = float(line[3])
+#     price = float(line[4])
+#     ans += quant*price
 
-format_ans = "{:.2f}".format(ans)
-print(format_ans)
+# format_ans = "{:.2f}".format(ans)
+# print(format_ans)
 
 #5
 open_file.close()
